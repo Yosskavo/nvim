249c217bf57a6472722ad0a64e1cd436fe5380d2
Soufiane El Abboubi <selabboubi55@gmail.com>
2026-03-23 23:51:01 +0100

    customize telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,26 +3,40 @@ local M = {}
 function M.setup()
 
 require('telescope').setup{
-	defaults = {
-		selection_caret = "󰁙 ",
-		mappings = {
-			i = {
-				["<C-h>"] = "which_key"
-			}
-		}
-	},
-	pickers = {
-		find_files = {
-			prompt_prefix = "󰈞 ",
-			prompt_title = "search for a file"
-		},
-		live_grep = {
-			prompt_prefix = "󱁊 ",
-			prompt_title = "search for a word"
-		}
-	},
-	extensions = {
-	},
+    defaults = {
+        selection_caret = "   ",
+        entry_prefix = " ",
+        sorting_strategy = "ascending",
+        layout_config = {
+              horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+              },
+            width = 0.87,
+              height = 0.80,
+        },
+        mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+            i = { ["<C-h>"] = "which_key"},
+        },
+    },
+    pickers = {
+        find_files = {
+            prompt_prefix = " 󰈞 ",
+            prompt_title = "search for a file",
+            default_text = "",
+        },
+        live_grep = {
+            prompt_prefix = " 󱁊 ",
+            prompt_title = "search for a word",
+            default_text = "",
+        },
+    },
+    extensions = {
+      live_grep_args = {
+        auto_quoting = true,
+      },
+    },
 }
 
 end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,12 @@
 return {
     'nvim-telescope/telescope.nvim',
-	enabled = true,
-	config = function()
-		require("config.telescope").setup()
-	end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    cmd = "Telescope",
+    config = function()
+        require("config.telescope").setup()
+        require('telescope').load_extension("live_grep_args")
+    end,
 }
