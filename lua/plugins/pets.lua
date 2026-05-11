ce5641b2d02f5b1b66d8c01483485e608b237606
Yassine El motaouakkel <yel-mota@c2r8p10.1337.ma>
2026-05-11 16:33:48 +0100

    remove pets

--- lua/plugins/pets.lua
+++ lua/plugins/pets.lua
@@ -1,13 +1,13 @@
 return {
-  "giabari/pets.nvim",
-  dependencies = {
-    "edluffy/hologram.nvim", -- Use this instead of holo.nvim
-  },
-  config = function()
-require("pets").setup({
-	  -- default_pet = "dog",   -- Set this to "dog", "cat", "slime", or "crab"
-	  -- default_style = "brown", -- Choose the specific color style
-	  random = false,        -- Disable random spawning to keep it consistent
-})
-  end,
+--   "giabari/pets.nvim",
+--   dependencies = {
+--     "edluffy/hologram.nvim", -- Use this instead of holo.nvim
+--   },
+--   config = function()
+-- require("pets").setup({
+-- 	  -- default_pet = "dog",   -- Set this to "dog", "cat", "slime", or "crab"
+-- 	  -- default_style = "brown", -- Choose the specific color style
+-- 	  random = false,        -- Disable random spawning to keep it consistent
+-- })
+--   end,
 }
