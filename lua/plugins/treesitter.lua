f404603a101f3a76e69d77378a150032f6f832c8
polipagram <bouarfakawtar68@gmail.com>
2026-05-17 17:13:38 +0100

    fix tree-sitter for nvim version 0.12.0

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,35 @@
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   -- tag = "v0.9.3", 
+--   build = ":TSUpdate",
+--   config = function()
+--     require("nvim-treesitter.configs").setup({
+--       -- 1. Make sure "bash" is here, and REMOVE "zsh"
+--       ensure_installed = { "c", "cpp", "lua", "bash", "markdown", "markdown_inline", "vim", "vimdoc" },
+--       highlight = { enable = true },
+--       indent = { enable = true },
+--     })
+--
+--     -- 2. Tell Neovim to use the bash parser whenever you open a zsh file
+--     vim.treesitter.language.register("bash", "zsh")
+--   end,
+-- }
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  tag = "v0.9.3", 
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- 1. Make sure "bash" is here, and REMOVE "zsh"
-      ensure_installed = { "c", "cpp", "lua", "bash", "markdown", "markdown_inline", "vim", "vimdoc" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+  -- Use "opts" instead of "config = function() require('nvim-treesitter.configs').setup(...) end"
+  opts = {
+    -- Make sure "bash" is here, and REMOVE "zsh"
+    ensure_installed = { "c", "cpp", "lua", "bash", "markdown", "markdown_inline", "vim", "vimdoc" },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    -- This handles the configuration automatically without needing the old module
+    require("nvim-treesitter").setup(opts)
 
-    -- 2. Tell Neovim to use the bash parser whenever you open a zsh file
+    -- Tell Neovim to use the bash parser whenever you open a zsh file
     vim.treesitter.language.register("bash", "zsh")
   end,
 }
