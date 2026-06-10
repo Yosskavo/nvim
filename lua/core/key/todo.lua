c480ae25ea543ff16d8efc781a381c4f661933f0
Yassine El motaouakkel <yel-mota@c1r4p3.1337.ma>
2026-06-10 14:33:46 +0100

    delete Unessacery todo finder

--- lua/core/key/todo.lua
+++ lua/core/key/todo.lua
@@ -1,3 +1,2 @@
 --- for todo-comments
-vim.keymap.set("n", "<leader>tf", ":TodoLocList<CR>", {desc = "find todo-comments", silent = true})
 vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", {desc = "find todo-comments", silent = true})
