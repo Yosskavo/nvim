06b09f363f9ea43e0e07cb6b1a464aa04c9bb371
Yosskavo <yosskavo@proton.me>
2026-07-04 10:52:56 +0100

    autosave

--- lua/core/cmd/autosave.lua
+++ lua/core/cmd/autosave.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
         -- Check if buffer is modified, is a normal file, and has a filename
         if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
             -- Use pcall (protected call) to prevent error messages from popping up
-			require("notify")("auto-save")
+			-- require("notify")("auto-save")
             pcall(vim.api.nvim_command, "silent! write")
         end
     end,
