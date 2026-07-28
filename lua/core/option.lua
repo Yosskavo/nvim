8506b6fb5abd0a2d1f4791646720be7e569cb77b
yosskavo <yosskavo@proton.me>
2026-07-28 10:47:11 +0100

    some changes

--- lua/core/option.lua
+++ lua/core/option.lua
@@ -20,7 +20,6 @@ vim.opt.relativenumber = true
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 
-
 -- this is for line hightlight
 vim.opt.cursorline = true
 
@@ -46,15 +45,8 @@ vim.opt.undofile = true
 -- this is for the theme's
 vim.lsp.semantic_tokens.enable = false
 
--- tabline
--- vim.o.showtabline = 2
--- vim.opt.sessionoptions = 'curdir,folds,globals,help,tabpages,terminal,winsize'
--- vim.opt.hidden = true
-
 -- Sets the delay for CursorHold (and swap file writing) to 250ms
 vim.opt.updatetime = 0
--- vim.o.winborder = "rounded"
-
 
 -- this is for characters
 vim.opt.nrformats:append("alpha")
@@ -68,3 +60,62 @@ vim.opt.shortmess:append("c")
 -- Allow the cursor to move into empty space in Visual Block mode
 vim.opt.virtualedit = "block"
 vim.deprecate = function() end
+
+-- ==========================================
+-- 🌌 TRANSPARENCY CONFIGURATION & :TP COMMAND
+-- ==========================================
+vim.g.transparent_enabled = true
+vim.g.tokyonight_transparent = true
+vim.g.catppuccin_transparent_background = true
+vim.g.transparent_background = true
+
+local transparent_groups = {
+    "Normal", "NormalNC", "SignColumn", "NormalFloat", "FloatBorder",
+    "TelescopeNormal", "TelescopeBorder", "NvimTreeNormal", "NvimTreeNormalNC"
+}
+
+local function apply_transparency(enable)
+    vim.g.transparent_enabled = enable
+    if enable then
+        for _, g in ipairs(transparent_groups) do
+            vim.cmd("highlight " .. g .. " ctermbg=NONE guibg=NONE")
+        end
+    else
+        local cs = vim.g.colors_name or "default"
+        vim.cmd("colorscheme " .. cs)
+    end
+end
+
+-- Auto-apply transparency whenever a colorscheme is loaded
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        if vim.g.transparent_enabled then
+            apply_transparency(true)
+        end
+    end,
+})
+
+-- User command :TP [on|off|toggle]
+vim.api.nvim_create_user_command("TP", function(opts)
+    local cmd_arg = tostring(opts.args or ""):lower()
+    if cmd_arg == "on" or cmd_arg == "enable" then
+        apply_transparency(true)
+        vim.notify("Neovim Transparency: ON", vim.log.levels.INFO)
+    elseif cmd_arg == "off" or cmd_arg == "disable" then
+        apply_transparency(false)
+        vim.notify("Neovim Transparency: OFF", vim.log.levels.INFO)
+    else
+        apply_transparency(not vim.g.transparent_enabled)
+        local status = vim.g.transparent_enabled and "ON" or "OFF"
+        vim.notify("Neovim Transparency: " .. status, vim.log.levels.INFO)
+    end
+end, {
+    nargs = "?",
+    complete = function()
+        return { "on", "off", "toggle" }
+    end,
+})
+
+-- Initial application on startup
+apply_transparency(true)
--- lua/theme/shekai.lua
+++ lua/theme/shekai.lua
@@ -1,4 +1,13 @@
-return
-{
-	"Yosskavo/shekai.nvim"
+return {
+    "yosskavo/shekai.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true, -- Enable background transparency
+        blur = true,        -- Enable soft blur background colors
+    },
+    config = function(_, opts)
+        require("shekai").setup(opts)
+        vim.cmd("colorscheme shekai")
+    end,
 }
