80bd8c683666b1e9364c4144e4cdad41f7b3b201
yosskavo <yosskavo@proton.me>
2026-07-29 14:41:55 +0100

    fix

--- lua/theme/shekai.lua
+++ lua/theme/shekai.lua
@@ -1,13 +1,10 @@
 return {
     "yosskavo/shekai.nvim",
-    lazy = false,
-    priority = 1000,
     opts = {
         transparent = true, -- Enable background transparency
         blur = true,        -- Enable soft blur background colors
     },
     config = function(_, opts)
         require("shekai").setup(opts)
-        vim.cmd("colorscheme shekai")
     end,
 }
